e9f01cd70402ec62cb29a3e12d7b3752c22dbcf4
Yaroslav Dekhtiaruk <dehtiaruk@gmail.com>
2023-04-16 12:34:02 +0300

    feat: finish carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+	let result = false;
+	console.log(sampleActivity);
+	if ((typeof sampleActivity) == 'string' && sampleActivity !== '0' && sampleActivity !== '' && sampleActivity !== ' \n\t\r' && sampleActivity !== ' ') {
+		result = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) * (HALF_LIFE_PERIOD / Math.log(2)));
+	}
+
+	if (isNaN(result) || result < 0) {
+		return false;
+	}
+
+	return result;
 }
 
 module.exports = {
